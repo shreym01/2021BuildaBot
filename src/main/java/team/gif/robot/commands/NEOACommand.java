eadb60c49abb7849c616019a6289460fb129b961
Shrey Mathur <shreym01@outlook.com>
2021-11-10 21:54:04 -0600

    NEO

--- src/main/java/team/gif/robot/commands/NEOACommand.java
+++ src/main/java/team/gif/robot/commands/NEOACommand.java
@@ -1,15 +1,13 @@
 package team.gif.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import team.gif.robot.Globals;
 import team.gif.robot.subsystems.NEO;
 
-public class NEOCommand extends CommandBase {
+public class NEOACommand extends CommandBase {
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
 
-    public NEOCommand() {
+    public NEOACommand() {
     }
-
 
     // Called when the command is initially scheduled.
     @Override
@@ -19,8 +17,7 @@ public class NEOCommand extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        NEO.getInstance().setVoltage(2.5);
-        Globals.g_buttonControl = true;
+        NEO.getInstance().setPID(60);
     }
 
     // Returns true when the command should end.
@@ -32,7 +29,5 @@ public class NEOCommand extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        NEO.getInstance().setVoltage(0);
-        Globals.g_buttonControl = false;
     }
 }
--- src/main/java/team/gif/robot/subsystems/NEO.java
+++ src/main/java/team/gif/robot/subsystems/NEO.java
@@ -1,9 +1,8 @@
 package team.gif.robot.subsystems;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.Constants;
 import team.gif.robot.Robot;
 import team.gif.robot.RobotMap;
 
@@ -20,6 +19,7 @@ public class NEO extends SubsystemBase {
 
     private static final CANSparkMax NEO = new CANSparkMax(RobotMap.MOTOR_SPARKMAX_ONE, CANSparkMaxLowLevel.MotorType.kBrushless);
     private static final CANEncoder NEOEncoder = NEO.getEncoder();
+    private static final CANPIDController NEOPIDController = NEO.getPIDController();
 
     int stallMaxAmps = 40;
 
@@ -29,12 +29,18 @@ public class NEO extends SubsystemBase {
     NEO.enableVoltageCompensation(12);
     NEO.setIdleMode(CANSparkMax.IdleMode.kCoast);
     NEO.setSmartCurrentLimit(stallMaxAmps,stallMaxAmps);
+
+    NEOPIDController.setOutputRange(0, 1);
     }
     public void setVoltage(double voltage) {
         NEO.setVoltage(voltage);
     }
 
     public double getVelocity () { return NEOEncoder.getVelocity();}
+    public void setPID (double setPoint) {
+        NEOPIDController.setReference(setPoint, ControlType.kVelocity);
+    }
 
     public String getVelocity_Shuffleboard(){ return String.format("%12.0f",getVelocity());}
+
 }
