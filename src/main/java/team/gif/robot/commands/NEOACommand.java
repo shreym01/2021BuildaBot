a443d605447b11690389f2b5b07555c3483d4c08
Shrey Mathur <shreym01@outlook.com>
2021-11-15 21:59:32 -0600

    NEO

--- src/main/java/team/gif/robot/commands/NEOACommand.java
+++ src/main/java/team/gif/robot/commands/NEOACommand.java
@@ -1,6 +1,7 @@
 package team.gif.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.gif.robot.Globals;
 import team.gif.robot.subsystems.NEO;
 
 public class NEOACommand extends CommandBase {
@@ -17,6 +18,7 @@ public class NEOACommand extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        Globals.g_buttonControl = true;
         NEO.getInstance().setPID(60);
     }
 
@@ -29,5 +31,7 @@ public class NEOACommand extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        NEO.getInstance().setVoltage(0);
+        Globals.g_buttonControl = false;
     }
 }
